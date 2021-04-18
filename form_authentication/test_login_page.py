6504fe76ee50b512e154478b478de05f516e61a6
mateusz <mateusz.bzdzion@gmail.com>
2021-04-18 14:15:15 +0200

    Add test without password

--- form_authentication/test_login_page.py
+++ form_authentication/test_login_page.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from login_page import Login
-from secure_area import Secure_Area
-from login_error_page import Login_Error
+
+from pages.login_page import Login
+from pages.secure_area import Secure_Area
+from pages.login_error_page import Login_Error
 
 
 @pytest.fixture
@@ -26,11 +26,21 @@ def test_login_page_with_valid_data(setup):
     assert redirect_page.get_flash_message_attribute() == 'flash success'
 
 
-def test_login_page_without_valid_username(setup):
+def test_login_page_without_username(setup):
     page = setup
     page.get('https://the-internet.herokuapp.com/login')
     login_form = Login(page)
     login_form.username = ''
+    login_form.login()
+    error_page = Login_Error(page)
+    assert error_page.get_flash_message_attribute() == 'flash error'
+
+
+def test_login_page_without_password(setup):
+    page = setup
+    page.get('https://the-internet.herokuapp.com/login')
+    login_form = Login(page)
+    login_form.password = ''
     login_form.login()
     error_page = Login_Error(page)
     assert error_page.get_flash_message_attribute() == 'flash error'
