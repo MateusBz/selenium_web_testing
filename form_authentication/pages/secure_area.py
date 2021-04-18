7d1911268e93e084ec9c95e702ef474eec291951
mateusz <mateusz.bzdzion@gmail.com>
2021-04-18 10:41:32 +0200

    Create pages for login page

---
+++ form_authentication/pages/secure_area.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class Secure_Area():
+    def __init__(self, driver) -> None:
+        self.driver = driver
+        self.flash_locator = 'flash'
+    
+    def get_flash_message_attribute(self) -> str:
+        return self.driver.find_element(By.ID, self.flash_locator).get_attribute('class')
+
