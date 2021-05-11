db01c6c1c1989a818c7903a3c89c2f5ec0e4e1b9
mateusz <mateusz.bzdzion@gmail.com>
2021-05-11 21:38:28 +0200

    Change file to pom

--- form_authentication/test_login_page.py
+++ form_authentication/test_login_page.py
@@ -1,49 +1,30 @@
 import pytest
-import os
-from selenium import webdriver
-
-from pages.login_page import Login
+from pages.login_page import LoginPage
 from pages.secure_area import SecureArea
 from pages.login_error_page import LoginError
 
 
-@pytest.fixture
-def setup() -> None:
-    caps = {'browserName': os.getenv('BROWSER', 'firefox')}
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        desired_capabilities=caps
-    )
-
-    yield driver
-
-    driver.quit()
-
-
 def test_login_page_with_valid_data(setup):
-    page = setup
-    page.get('https://the-internet.herokuapp.com/login')
-    login_form = Login(page)
-    login_form.login()
-    redirect_page = SecureArea(page)
+    page = LoginPage(setup)
+    page.open('login')
+    page.login()
+    redirect_page = SecureArea(setup)
     assert redirect_page.get_flash_message_attribute() == 'flash success'
 
 
 def test_login_page_without_username(setup):
-    page = setup
-    page.get('https://the-internet.herokuapp.com/login')
-    login_form = Login(page)
-    login_form.username = ''
-    login_form.login()
-    error_page = LoginError(page)
+    page = LoginPage(setup)
+    page.open('login')
+    page.username = ''
+    page.login()
+    error_page = LoginError(setup)
     assert error_page.get_flash_message_attribute() == 'flash error'
 
 
 def test_login_page_without_password(setup):
-    page = setup
-    page.get('https://the-internet.herokuapp.com/login')
-    login_form = Login(page)
-    login_form.password = ''
-    login_form.login()
-    error_page = LoginError(page)
+    page = LoginPage(setup)
+    page.open('login')
+    page.password = ''
+    page.login()
+    error_page = LoginError(setup)
     assert error_page.get_flash_message_attribute() == 'flash error'
