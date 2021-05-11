49c26edd8768c4788ed7ba9c3809b0861b9ddfaf
mateusz <mateusz.bzdzion@gmail.com>
2021-05-11 21:07:52 +0200

    Fix test upload to pom

--- file_uploader/test_upload_page.py
+++ file_uploader/test_upload_page.py
@@ -1,27 +1,11 @@
 import pytest
-import os
-from selenium import webdriver
 from selenium.webdriver.remote.file_detector import LocalFileDetector
-from file_uploader.page import UploadPage
-
-
-@pytest.fixture
-def setup() -> None:
-    caps = {'browserName': os.getenv('BROWSER', 'firefox')}
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        desired_capabilities=caps
-    )
-
-    yield driver
-
-    driver.quit()
+from pages.upload_page import UploadPage
 
 
 def test_upload_page(setup):
     file_path = 'file_uploader/test_file.txt'
-    page = setup
+    page = UploadPage(setup)
     page.file_detector = LocalFileDetector()
-    page.get('https://the-internet.herokuapp.com/upload')
-    upload_page = UploadPage(page)
-    assert upload_page.upload(file_path) == 'test_file.txt'
+    page.open('upload')
+    assert page.upload(file_path) == 'test_file.txt'
